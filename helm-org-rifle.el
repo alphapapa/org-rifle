4b0b498f8a5661ed6219f0decec6f7f092aae6ec
Adam Porter <adam@alphapapa.net>
2016-03-26 22:34:52 -0500

    repunctuate-sentences

--- helm-org-rifle.el
+++ helm-org-rifle.el
@@ -92,28 +92,28 @@ to be a major bottleneck."
   :group 'helm-org-rifle :type 'boolean)
 
 (defun helm-org-rifle ()
-  "This is my rifle. There are many like it, but this one is mine.
+  "This is my rifle.  There are many like it, but this one is mine.
 
-My rifle is my best friend. It is my life. I must master it as I
+My rifle is my best friend.  It is my life.  I must master it as I
 must master my life.
 
-Without me, my rifle is useless. Without my rifle, I am
-useless. I must fire my rifle true. I must shoot straighter than
-my enemy who is trying to kill me. I must shoot him before he
-shoots me. I will...
+Without me, my rifle is useless.  Without my rifle, I am
+useless.  I must fire my rifle true.  I must shoot straighter than
+my enemy who is trying to kill me.  I must shoot him before he
+shoots me.  I will...
 
 My rifle and I know that what counts in war is not the rounds we
-fire, the noise of our burst, nor the smoke we make. We know that
-it is the hits that count. We will hit...
+fire, the noise of our burst, nor the smoke we make.  We know that
+it is the hits that count.  We will hit...
 
-My rifle is human, even as I, because it is my life. Thus, I will
-learn it as a brother. I will learn its weaknesses, its strength,
-its parts, its accessories, its sights and its barrel. I will
-keep my rifle clean and ready, even as I am clean and ready. We
-will become part of each other. We will...
+My rifle is human, even as I, because it is my life.  Thus, I will
+learn it as a brother.  I will learn its weaknesses, its strength,
+its parts, its accessories, its sights and its barrel.  I will
+keep my rifle clean and ready, even as I am clean and ready.  We
+will become part of each other.  We will...
 
-Before God, I swear this creed. My rifle and I are the defenders
-of my country. We are the masters of our enemy. We are the
+Before God, I swear this creed.  My rifle and I are the defenders
+of my country.  We are the masters of our enemy.  We are the
 saviors of my life.
 
 So be it, until victory is ours and there is no enemy, but
