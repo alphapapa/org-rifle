2230e7b58713d419dc196ad023303fc9ea164800
Adam Porter <adam@alphapapa.net>
2016-03-26 21:29:43 -0500

    This is my rifle.

--- helm-org-rifle.el
+++ helm-org-rifle.el
@@ -46,10 +46,49 @@ to be the bottleneck.")
   "Show the whole heading path instead of just the node's heading.")
 
 (defun helm-org-rifle ()
-  "Rifle!"
+  "This is my rifle. There are many like it, but this one is mine.
+
+My rifle is my best friend. It is my life. I must master it as I
+must master my life.
+
+Without me, my rifle is useless. Without my rifle, I am
+useless. I must fire my rifle true. I must shoot straighter than
+my enemy who is trying to kill me. I must shoot him before he
+shoots me. I will...
+
+My rifle and I know that what counts in war is not the rounds we
+fire, the noise of our burst, nor the smoke we make. We know that
+it is the hits that count. We will hit...
+
+My rifle is human, even as I, because it is my life. Thus, I will
+learn it as a brother. I will learn its weaknesses, its strength,
+its parts, its accessories, its sights and its barrel. I will
+keep my rifle clean and ready, even as I am clean and ready. We
+will become part of each other. We will...
+
+Before God, I swear this creed. My rifle and I are the defenders
+of my country. We are the masters of our enemy. We are the
+saviors of my life.
+
+So be it, until victory is ours and there is no enemy, but
+peace!"
   (interactive)
   (let ((helm-candidate-separator " "))
     (helm :sources (helm-org-rifle-get-sources))))
+
+(defun helm-org-rifle-show-in-indirect-buffer (candidate)
+  (let ((buffer (helm-attr 'buffer))
+        (pos (car candidate))
+        (original-buffer (current-buffer)))
+    (switch-to-buffer buffer)
+    (goto-char pos)
+    (org-tree-to-indirect-buffer)
+    (unless (equal original-buffer (car (window-prev-buffers)))
+      ;; The selected bookmark was in a different buffer.  Put the
+      ;; non-indirect buffer at the bottom of the prev-buffers list
+      ;; so it won't be selected when the indirect buffer is killed.
+      (set-window-prev-buffers nil (append (cdr (window-prev-buffers))
+                                           (car (window-prev-buffers)))))))
 
 (defun helm-org-rifle-get-sources ()
   "Return list of sources configured for helm-org-rifle,
