13c15b7f9e67ceb2ee078fbe16a506af3e5548bd
Adam Porter <adam@alphapapa.net>
2018-01-15 03:06:46 -0600

    1.5.0 post-release update

--- helm-org-rifle.el
+++ helm-org-rifle.el
@@ -2,7 +2,7 @@
 
 ;; Author: Adam Porter <adam@alphapapa.net>
 ;; Url: http://github.com/alphapapa/helm-org-rifle
-;; Version: 1.5.0
+;; Version: 1.6.0-pre
 ;; Package-Requires: ((emacs "24.4") (dash "2.12") (f "0.18.1") (helm "1.9.4") (s "1.10.0"))
 ;; Keywords: hypermedia, outlines
 
